6f03f6d3d48f85d214885d82a5469c7708b52e25
Pascual Cano <pascual@allhuman.com>
2023-05-23 22:05:04 +0200

    Using collaborators

--- src/date.utils.ts
+++ src/date.utils.ts
@@ -2,6 +2,15 @@ export const getDayName = (date: Date): string => {
   return date.toLocaleDateString("en-IE", { weekday: "long" });
 };
 
+export const getShortDate = (): string => {
+  return new Date().toLocaleDateString("en-IE");
+};
+
+export const printLine = (line: string): void => {
+  console.log(line);
+};
+
 export const printDate = (): void => {
-  console.log(new Date().toLocaleDateString("en-IE"));
+  const shortDate = getShortDate();
+  printLine(shortDate);
 };
--- tests/date.utils.test.ts
+++ tests/date.utils.test.ts
@@ -1,9 +1,15 @@
-import { printDate } from "../src/date.utils";
+import * as utils from "../src/date.utils";
+const { printDate, getShortDate, printLine } = utils;
 
-describe("printDate", () => {
-  const fixedDate = new Date("2021-01-01");
-  const dateSpy = jest.spyOn(global, "Date").mockImplementation(() => fixedDate);
-  const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => ({ log: jest.fn() }));
+describe("printDate spies tests", () => {
+  let dateSpy: jest.SpyInstance, consoleSpy: jest.SpyInstance;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const fixedDate = new Date("2021-01-01");
+    dateSpy = jest.spyOn(global, "Date").mockImplementation(() => fixedDate);
+    consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
   it("creates a new date", () => {
     printDate();
     expect(dateSpy).toHaveBeenCalled();
@@ -11,5 +17,43 @@ describe("printDate", () => {
   it("console logs the mocked date", () => {
     printDate();
     expect(consoleSpy).toBeCalledWith("1/1/2021");
+  });
+});
+
+describe("printDate spies  collaborators tests", () => {
+  let shortDateSpy: jest.SpyInstance, printLineSpy: jest.SpyInstance;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const fixedDate = new Date("2021-01-01");
+    shortDateSpy = jest.spyOn(utils, "getShortDate").mockImplementation(() => "1/1/2021");
+    printLineSpy = jest.spyOn(utils, "printLine").mockImplementation(() => {});
+  });
+
+  it("creates a new date", () => {
+    printDate();
+    expect(shortDateSpy).toHaveBeenCalled();
+  });
+  it("console logs the mocked date", () => {
+    printDate();
+    expect(printLineSpy).toBeCalledWith("1/1/2021");
+  });
+});
+
+describe("testing collaborators", () => {
+  let consoleSpy: jest.SpyInstance;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const fixedDate = new Date("2021-01-01");
+    jest.spyOn(global, "Date").mockImplementation(() => fixedDate);
+    consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+  it("getShortDate formats correctly current date", () => {
+    const shortDate = getShortDate();
+    expect(shortDate).toBe("1/1/2021");
+  });
+
+  it("printLine console logs the input", () => {
+    printLine("hello");
+    expect(consoleSpy).toBeCalledWith("hello");
   });
 });
