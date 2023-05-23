4651882ca05165df650d2351d35690a3a0e33cad
Pascual Cano <pascual@allhuman.com>
2023-05-23 21:24:59 +0200

    Use spies to test printDate

--- tests/date.utils.test.ts
+++ tests/date.utils.test.ts
@@ -1,7 +1,15 @@
 import { printDate } from "../src/date.utils";
 
 describe("printDate", () => {
-  it("find a proper name for this test", () => {
+  const fixedDate = new Date("2021-01-01");
+  const dateSpy = jest.spyOn(global, "Date").mockImplementation(() => fixedDate);
+  const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => ({ log: jest.fn() }));
+  it("creates a new date", () => {
     printDate();
+    expect(dateSpy).toHaveBeenCalled();
+  });
+  it("console logs the mocked date", () => {
+    printDate();
+    expect(consoleSpy).toBeCalledWith("1/1/2021");
   });
 });
