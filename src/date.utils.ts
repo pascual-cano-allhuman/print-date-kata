34787f6142ef2096704ba3bf72836735300890ab
Pascual Cano <pascual@allhuman.com>
2023-05-26 16:07:26 +0200

    Creating custom spies with dependency injection

--- src/date.utils.ts
+++ src/date.utils.ts
@@ -17,3 +17,43 @@ export const formatDate = (date: Date) => {
 export const printLine = (line: string) => {
   console.log(line);
 };
+
+export interface IDateManager {
+  getCurrentDate(): Date;
+  formatDate(date: Date): string;
+}
+
+export interface IPrintManager {
+  printLine(line: string): void;
+}
+
+export class PrintDate {
+  dateManager: IDateManager;
+  printManager: IPrintManager;
+
+  constructor(dateManager: IDateManager, printManager: IPrintManager) {
+    this.dateManager = dateManager;
+    this.printManager = printManager;
+  }
+
+  printDate(): void {
+    const date = this.dateManager.getCurrentDate();
+    const formattedDate = this.dateManager.formatDate(date);
+    this.printManager.printLine(formattedDate);
+  }
+}
+
+export class DateManager implements IDateManager {
+  getCurrentDate = () => {
+    return new Date();
+  };
+  formatDate = (date: Date) => {
+    return date.toLocaleDateString("en-IE");
+  };
+}
+
+export class PrintManager implements IPrintManager {
+  printLine = (line: string) => {
+    console.log(line);
+  };
+}
--- tests/date.utils.test.ts
+++ tests/date.utils.test.ts
@@ -29,13 +29,13 @@ describe("printDate_2", () => {
   let spyFormatDate: jest.SpyInstance;
   let spyPrintLine: jest.SpyInstance;
   const mockDate = new Date("1978-12-18");
-    
+
   beforeEach(() => {
     jest.clearAllMocks();
 
     spyCurrentDate = jest.spyOn(Utils, "getCurrentDate").mockImplementation(() => mockDate);
     spyFormatDate = jest.spyOn(Utils, "formatDate").mockImplementation(() => "1978-12-18");
-    spyPrintLine = jest.spyOn(Utils, "printLine").mockImplementation(() => {})
+    spyPrintLine = jest.spyOn(Utils, "printLine").mockImplementation(() => {});
   });
 
   it("the current date is created", () => {
@@ -47,11 +47,60 @@ describe("printDate_2", () => {
     printDate();
     expect(spyFormatDate).toHaveBeenCalledTimes(1);
     expect(spyFormatDate).toHaveBeenCalledWith(mockDate);
-  })
+  });
 
   it("the date is printed", () => {
     printDate();
     expect(spyPrintLine).toHaveBeenCalledTimes(1);
     expect(spyPrintLine).toHaveBeenCalledWith("1978-12-18");
-  })
+  });
+});
+
+class PrintManagerSpy implements Utils.IPrintManager {
+  linesPassed: string[] = [];
+  printLine = (line: string) => {
+    this.linesPassed.push(line);
+  };
+}
+
+class DateManagerSpy implements Utils.IDateManager {
+  currentDateHasBeenCalled = false;
+  datesPassed: Date[] = [];
+
+  getCurrentDate = () => {
+    this.currentDateHasBeenCalled = true;
+    return new Date("1978-12-18");
+  };
+  formatDate = (date: Date) => {
+    this.datesPassed.push(date);
+    return "18-12-1978";
+  };
+}
+
+describe("PrintDate class", () => {
+  let printManager: PrintManagerSpy;
+  let dateManager: DateManagerSpy;
+  let printDate: Utils.PrintDate;
+
+  beforeEach(() => {
+    printManager = new PrintManagerSpy();
+    dateManager = new DateManagerSpy();
+    printDate = new Utils.PrintDate(dateManager, printManager);
+  });
+
+  it("should create a date", () => {
+    printDate.printDate();
+    expect(dateManager.currentDateHasBeenCalled).toBe(true);
+  });
+
+  it("should format a date", () => {
+    printDate.printDate();
+    expect(dateManager.datesPassed[0]).toEqual(new Date("1978-12-18"));
+  });
+
+  it("a line should be printed", () => {
+    printDate.printDate();
+    expect(printManager.linesPassed.length).toBe(1);
+    expect(printManager.linesPassed[0]).toBe("18-12-1978");
+  });
 });
