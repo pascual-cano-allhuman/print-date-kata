3d7cf6f239e0ed06ca29a1a8af5bb4efbf7594f9
Pascual Cano <pascual@allhuman.com>
2023-05-26 12:09:26 +0200

    Spying on Date and console.log tests

--- tests/date.utils.test.ts
+++ tests/date.utils.test.ts
@@ -1,7 +1,24 @@
 import { printDate } from "../src/date.utils";
 
 describe("printDate", () => {
-  it("find a proper name for this test", () => {
+  let spyDate: jest.SpyInstance;
+  let spyLog: jest.SpyInstance;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    const mockDate = new Date("1978-12-18");
+    spyDate = jest.spyOn(global, "Date").mockImplementation(() => mockDate);
+    spyLog = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
+  it("the current date is created", () => {
     printDate();
+    expect(spyDate).toHaveBeenCalledTimes(1);
+  });
+
+  it("the current date is printed", () => {
+    printDate();
+    expect(spyLog).toHaveBeenCalledTimes(1);
+    expect(spyLog).toHaveBeenCalledWith("18/12/1978");
   });
 });
