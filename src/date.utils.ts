f47542babba3499d47fe4e9f49f227d95d9fe3b7
stelly <stelly@localhost.com>
2023-05-26 12:39:50 +0200

    testing collaborators

--- src/date.utils.ts
+++ src/date.utils.ts
@@ -1,19 +1,19 @@
 export const getDayName = (date: Date): string => {
-    return date.toLocaleDateString("en-IE", {weekday: "long"});
+  return date.toLocaleDateString("en-IE", { weekday: "long" });
 };
 
 export const printDate = (): void => {
-    const date = getCurrentDate();
-    const formattedDate = formatDate(date);
-    printLine(formattedDate);
+  const date = getCurrentDate();
+  const formattedDate = formatDate(date);
+  printLine(formattedDate);
 };
 
-const getCurrentDate = () => {
-    return new Date();
-}
-const formatDate = (date: Date) => {
-    return date.toLocaleDateString("en-IE");
-}
-const printLine = (line: string) => {
-    return console.log(line);
-}
+export const getCurrentDate = () => {
+  return new Date();
+};
+export const formatDate = (date: Date) => {
+  return date.toLocaleDateString("en-IE");
+};
+export const printLine = (line: string) => {
+  console.log(line);
+};
--- tests/date.utils.test.ts
+++ tests/date.utils.test.ts
@@ -1,4 +1,5 @@
 import { printDate } from "../src/date.utils";
+import * as Utils from "../src/date.utils";
 
 describe("printDate", () => {
   let spyDate: jest.SpyInstance;
@@ -21,4 +22,36 @@ describe("printDate", () => {
     expect(spyLog).toHaveBeenCalledTimes(1);
     expect(spyLog).toHaveBeenCalledWith("18/12/1978");
   });
+});
+
+describe("printDate_2", () => {
+  let spyCurrentDate: jest.SpyInstance;
+  let spyFormatDate: jest.SpyInstance;
+  let spyPrintLine: jest.SpyInstance;
+  const mockDate = new Date("1978-12-18");
+    
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    spyCurrentDate = jest.spyOn(Utils, "getCurrentDate").mockImplementation(() => mockDate);
+    spyFormatDate = jest.spyOn(Utils, "formatDate").mockImplementation(() => "1978-12-18");
+    spyPrintLine = jest.spyOn(Utils, "printLine").mockImplementation(() => {})
+  });
+
+  it("the current date is created", () => {
+    printDate();
+    expect(spyCurrentDate).toHaveBeenCalledTimes(1);
+  });
+
+  it("the date is formatted", () => {
+    printDate();
+    expect(spyFormatDate).toHaveBeenCalledTimes(1);
+    expect(spyFormatDate).toHaveBeenCalledWith(mockDate);
+  })
+
+  it("the date is printed", () => {
+    printDate();
+    expect(spyPrintLine).toHaveBeenCalledTimes(1);
+    expect(spyPrintLine).toHaveBeenCalledWith("1978-12-18");
+  })
 });
